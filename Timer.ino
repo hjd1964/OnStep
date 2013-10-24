a52f0a21552333e0392687701537f684f2aa15f3
Howard Dutton <hjd1964@gmail.com>
2013-10-24 13:25:43 -0400

    Update Timer.ino
    Fixed HA/RA and Dec ability to reverse direction in initialization.

--- Timer.ino
+++ Timer.ino
@@ -22,7 +22,11 @@ ISR(TIMER1_COMPA_vect)
   skipHA++;                                          // Rate control RA
   if (skipHA>=skipCountHA) {                         
     if (posHA<(targetHA+PEC_HA)) dirHA=1; else dirHA=0;                        // Direction control
+#ifdef ReverseHA
+    if (HADir==dirHA) CLR(HADirPORT, HADirBit); else SET(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+#else
     if (HADir==dirHA) SET(HADirPORT, HADirBit); else CLR(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+#endif
     skipHA=0;
 
     if (posHA!=(targetHA+PEC_HA)) {                  // Move the RA stepper to the target
@@ -44,7 +48,11 @@ ISR(TIMER1_COMPA_vect)
 
     // telescope normally starts on the EAST side of the pier looking at the WEST sky
     if (posDec<targetDec) dirDec=1; else dirDec=0;                                   // Direction control
+#ifdef ReverseDec
+    if (DecDir==dirDec) SET(DecDirPORT, DecDirBit); else CLR(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+#else
     if (DecDir==dirDec) CLR(DecDirPORT, DecDirBit); else SET(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+#endif
     skipDec=0;
 
     if (posDec!=targetDec) {                         // move the Dec stepper to the target
