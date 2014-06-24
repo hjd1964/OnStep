31b7eabdc648ba3076063f8a31ff2723285245ab
Howard Dutton <hjd1964@gmail.com>
2014-06-24 18:40:13 -0400

    Changes to eliminate delay in isr on Teensy

--- Timer.ino
+++ Timer.ino
@@ -2,6 +2,8 @@
 #define ISR(f) void f (void)
 IntervalTimer itimer1;
 void TIMER1_COMPA_vect(void);
+volatile boolean HAclr = true;
+volatile boolean DEclr = true;
 #endif
 
 //--------------------------------------------------------------------------------------------------
@@ -33,27 +35,29 @@ void Timer1SetRate(long rate) {
 #endif
 }
 
-// set timer3 to rate (in microseconds*16) valid for rates down to 1/4 second
+// set timer3 to rate (in microseconds*16)
 volatile uint32_t nextHArate;
 void Timer3SetRate(long rate) {
 #if defined(__AVR__)
+  // valid for rates down to 1/4 second
   rate=rate/64;
   nextHArate=rate-1;
 #elif defined(__arm__) && defined(TEENSYDUINO)
-  nextHArate=(F_BUS / 1000000) * (rate*0.0625) - 1;
+  nextHArate=(F_BUS / 1000000) * (rate*0.0625) * 0.5 - 1;
   cli();
   PIT_LDVAL1=nextHArate;
   sei();
 #endif
 }
-// set timer4 to rate (in microseconds*16) valid for rates down to 1/4 second
+// set timer4 to rate (in microseconds*16)
 volatile uint32_t nextDErate;
 void Timer4SetRate(long rate) {
 #if defined(__AVR__)
+  // valid for rates down to 1/4 second
   rate=rate/64;
   nextDErate=rate-1;
 #elif defined(__arm__) && defined(TEENSYDUINO)
-  nextDErate=(F_BUS / 1000000) * (rate*0.0625) - 1;
+  nextDErate=(F_BUS / 1000000) * (rate*0.0625) * 0.5 - 1;
   cli();
   PIT_LDVAL2=nextDErate;
   sei();
@@ -76,7 +80,6 @@ ISR(TIMER1_COMPA_vect)
       Timer3SetRate(thisTimerRate);
       isrTimerRateHA=thisTimerRate;
     }
-
     thisTimerRate=timerRateDec;
     // override rate during backlash compensation
     if (inBacklashDec) thisTimerRate=timerRateBacklashDec;
@@ -98,24 +101,31 @@ ISR(TIMER1_COMPA_vect)
 
 ISR(TIMER3_COMPA_vect)
 {
-  // routine might run in say 5uS?
+#if defined(__AVR__)
   // drivers step on the rising edge, they need 1uS to settle so this is early in the routine
   CLR(HAStepPORT,  HAStepBit);
-
-  if (posHA<(targetHA+PEC_HA)) dirHA=1; else dirHA=0;                        // Direction control
-#ifdef REVERSE_HA_ON
-  if (HADir==dirHA) CLR(HADirPORT, HADirBit); else SET(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
-#else
-  if (HADir==dirHA) SET(HADirPORT, HADirBit); else CLR(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+  if (posHA<(targetHA+PEC_HA)) dirHA=1; else dirHA=0; // Direction control
+  #ifdef REVERSE_HA_ON
+    if (HADir==dirHA) CLR(HADirPORT, HADirBit); else SET(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+  #else
+    if (HADir==dirHA) SET(HADirPORT, HADirBit); else CLR(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+  #endif
+#elif defined(__arm__) && defined(TEENSYDUINO)
+  // on the much faster Teensy run at twice the rate and pull the step pin low every other call
+  if (HAclr) {
+    CLR(HAStepPORT,  HAStepBit);
+    if (posHA<(targetHA+PEC_HA)) dirHA=1; else dirHA=0; // Direction control
+    #ifdef REVERSE_HA_ON
+      if (HADir==dirHA) CLR(HADirPORT, HADirBit); else SET(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+    #else
+      if (HADir==dirHA) SET(HADirPORT, HADirBit); else CLR(HADirPORT, HADirBit); // Set direction, HADir default LOW (=0, for my wiring)
+    #endif
+    HAclr=false;
+    return;
+  } else HAclr=true;
 #endif
-
-#ifdef defined(__arm__) && defined(TEENSYDUINO)
-  // waste some time on faster MPU's to keep the stepper driver's in-spec
-  delayMicroseconds(2);
-#endif
-
-  if (posHA!=(targetHA+PEC_HA)) {                                            // Move the RA stepper to the target
-
+  
+  if (posHA!=(targetHA+PEC_HA)) { // Move the RA stepper to the target
     // telescope moves WEST with the sky, blHA is the amount of EAST backlash
     if (dirHA==1) {
      if (blHA<backlashHA) { blHA++; inBacklashHA=true; } else { inBacklashHA=false; posHA++; }
@@ -132,25 +142,33 @@ ISR(TIMER3_COMPA_vect)
 
 ISR(TIMER4_COMPA_vect)
 {
-  // routine might run in say 5uS?
+#if defined(__AVR__)
   // drivers step on the rising edge, they need 1uS to settle so this is early in the routine
-  CLR(DecStepPORT, DecStepBit);
-  
+  CLR(DecStepPORT,  DecStepBit);
   // telescope normally starts on the EAST side of the pier looking at the WEST sky
-  if (posDec<targetDec) dirDec=1; else dirDec=0;                                   // Direction control
-#ifdef REVERSE_DEC_ON
-  if (DecDir==dirDec) SET(DecDirPORT, DecDirBit); else CLR(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
-#else
-  if (DecDir==dirDec) CLR(DecDirPORT, DecDirBit); else SET(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
-#endif
-
-#ifdef defined(__arm__) && defined(TEENSYDUINO)
-  // waste some time on faster MPU's to keep the stepper driver's in-spec
-  delayMicroseconds(2);
+  if (posDec<targetDec) dirDec=1; else dirDec=0;                                     // Direction control
+  #ifdef REVERSE_DEC_ON
+    if (DecDir==dirDec) SET(DecDirPORT, DecDirBit); else CLR(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+  #else
+    if (DecDir==dirDec) CLR(DecDirPORT, DecDirBit); else SET(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+  #endif
+#elif defined(__arm__) && defined(TEENSYDUINO)
+  // on the much faster Teensy run this ISR at twice the normal rate and pull the step pin low every other call
+  if (DEclr) {
+    CLR(DecStepPORT,  DecStepBit);
+    // telescope normally starts on the EAST side of the pier looking at the WEST sky
+    if (posDec<targetDec) dirDec=1; else dirDec=0;                                     // Direction control
+    #ifdef REVERSE_DEC_ON
+      if (DecDir==dirDec) SET(DecDirPORT, DecDirBit); else CLR(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+    #else
+      if (DecDir==dirDec) CLR(DecDirPORT, DecDirBit); else SET(DecDirPORT, DecDirBit); // Set direction, decDir default HIGH (=1, for my wiring)
+    #endif
+    DEclr=false;
+    return;
+  } else DEclr=true;
 #endif
 
   if (posDec!=targetDec) {                                                         // move the Dec stepper to the target
-
     // telescope moving NORTH in the sky, blDec is the amount of SOUTH backlash
     if (dirDec==1) {
       if (blDec<backlashDec) { blDec++; inBacklashDec=true; } else { inBacklashDec=false; posDec++; }
