4fdc9a8bc57ef6ecddb17dfbe6d7ffc1795a5566
Howard Dutton <hjd1964@gmail.com>
2015-05-07 08:49:52 -0400

    Create Library.h

---
+++ Library.h
@@ -0,0 +1,57 @@
+// -----------------------------------------------------------------------------------
+// Object Libraries
+
+#include "Arduino.h"
+
+const int rec_size = 16;
+typedef struct libRecBase_t{
+  char name[7];  // 7
+  byte code;     // 1 (low 4 bits are object class, high are catalog #)
+  float RA;      // 4
+  float Dec;     // 4
+};
+
+typedef union libRec_t{
+  libRecBase_t libRec;
+  byte libRecBytes[rec_size];
+};
+
+class Library
+{
+  public:
+    Library();
+    ~Library();
+    
+    boolean setCatalog(int num);
+
+    // 16 byte record
+    libRec_t list;
+    
+    void writeVars(char* name, int code, double RA, double Dec);
+    void readVars(char* name, int* code, double* RA, double* Dec);
+
+    boolean firstRec();
+    boolean firstFreeRec();
+    boolean prevRec();
+    boolean nextRec();
+    boolean gotoRec(int num);
+
+    void clearLib(); // clears this library
+    void clearAll(); // clears all libraries
+
+    int recCount(); // actual number of records for this library
+    int recPos;     // currently selected record#
+    int recMax;     // last record#
+    
+  private:
+    libRec_t readRec(int address);
+    void writeRec(int address, libRec_t data);
+    void clearRec(int address);
+
+    int catalog;
+
+    int byteCount;
+    int bytePos;
+    int byteMin;
+    int byteMax;
+};
