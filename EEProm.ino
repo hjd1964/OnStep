ad48c81c30aeb27455babb6875c13a9885688cc0
Howard Dutton <hjd1964@gmail.com>
2015-08-18 20:36:22 -0400

    Update EEProm.ino

--- EEProm.ino
+++ EEProm.ino
@@ -4,8 +4,8 @@
 // write int numbers into EEPROM at position i (2 bytes)
 void EEPROM_writeInt(int i,int j) {
   uint8_t *k = (uint8_t*)&j;
-  EEPROM.write(i+0,*k); k++;
-  EEPROM.write(i+1,*k);
+  EEPROM.update(i+0,*k); k++;
+  EEPROM.update(i+1,*k);
 }
 
 // read int numbers from EEPROM at position i (2 bytes)
@@ -19,10 +19,10 @@ int EEPROM_readInt(int i) {
 
 // write 4 byte variable into EEPROM at position i (4 bytes)
 void EEPROM_writeQuad(int i,byte *v) {
-  EEPROM.write(i+0,*v); v++;
-  EEPROM.write(i+1,*v); v++;
-  EEPROM.write(i+2,*v); v++;
-  EEPROM.write(i+3,*v);
+  EEPROM.update(i+0,*v); v++;
+  EEPROM.update(i+1,*v); v++;
+  EEPROM.update(i+2,*v); v++;
+  EEPROM.update(i+3,*v);
 }
 
 // read 4 byte variable from EEPROM at position i (4 bytes)
@@ -36,7 +36,7 @@ void EEPROM_readQuad(int i,byte *v) {
 // write String into EEPROM at position i (16 bytes)
 void EEPROM_writeString(int i,char l[]) {
   for (int l1=0; l1<16; l1++) {
-    EEPROM.write(i+l1,*l); l++;
+    EEPROM.update(i+l1,*l); l++;
   }
 }
 
