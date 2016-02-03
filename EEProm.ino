b6160413ff34b99d25e715072529c8b2aef45f44
Howard Dutton <hjd1964@gmail.com>
2016-02-03 14:07:29 -0500

    Refactoring, PEC improvements

--- EEProm.ino
+++ EEProm.ino
@@ -46,3 +46,27 @@ void EEPROM_readString(int i, char l[]) {
     *l=EEPROM.read(i+l1); l++;
   }
 }
+
+// write 4 byte float into EEPROM at position i (4 bytes)
+void EEPROM_writeFloat(int i,float f) {
+  EEPROM_writeQuad(i,(byte*)&f);
+}
+
+// read 4 byte float from EEPROM at position i (4 bytes)
+float EEPROM_readFloat(int i) {
+  float f;
+  EEPROM_readQuad(i,(byte*)&f);
+  return f;
+}
+
+// write 4 byte long into EEPROM at position i (4 bytes)
+void EEPROM_writeLong(int i,long l) {
+  EEPROM_writeQuad(i,(byte*)&l);
+}
+
+// read 4 byte long from EEPROM at position i (4 bytes)
+long EEPROM_readLong(int i) {
+  long l;
+  EEPROM_readQuad(i,(byte*)&l);
+  return l;
+}
