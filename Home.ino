bf71bc4cfb69c11efb131b2bb5dd3543b478bf14
Howard Dutton <hjd1964@gmail.com>
2016-04-18 09:36:12 -0400

    Added stepper driver initialization to setHome()

--- Home.ino
+++ Home.ino
@@ -112,6 +112,13 @@ boolean setHome() {
   guideDurationDec    = 0;
   guideDurationLastDec= 0;
 
+  // initialize/disable the stepper drivers
+#if defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+  DecayModeTracking();
+#endif
+  digitalWrite(Axis1_EN,Axis1_Disabled);
+  digitalWrite(Axis2_EN,Axis2_Disabled);
+
   cli();
   targetAxis1.part.m = startAxis1; targetAxis1.part.f = 0;
   posAxis1           = startAxis1;
