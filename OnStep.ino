63dc0c013f79458b7958efbd0fefaa0ce53531cd
Howard Dutton <hjd1964@gmail.com>
2017-07-26 16:44:40 -0400

    Updated revision

--- OnStep.ino
+++ OnStep.ino
@@ -67,7 +67,7 @@
 #endif
 
 // firmware info, these are returned by the ":GV?#" commands
-#define FirmwareDate   "02 27 17"
+#define FirmwareDate   "07 26 17"
 #define FirmwareNumber "1.0b"
 #define FirmwareName   "On-Step"
 #define FirmwareTime   "12:00:00"
