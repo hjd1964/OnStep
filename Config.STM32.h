62a510471d6046cac352bdb32bb2bdfe3606546b
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2017-12-25 13:23:56 -0500

    Default back to regular settings, without TMC2130, since I plan to use RAMPS.

--- Config.STM32.h
+++ Config.STM32.h
@@ -194,14 +194,14 @@
 // Another option allows setting 50% power while tracking (TMC_LOWPWR.)  As above, with this option on too:
 // AXIS1_MODE (3|TMC_STEALTHCHOP|TMC_LOWPWR)
 //
-#define AXIS1_MODE (3|TMC_STEALTHCHOP|TMC_LOWPWR)  // programs the RA/Az uStep mode M0/M1/M2, optional and default _OFF.
-#define AXIS1_MODE_GOTO 1          // programs the RA/Az uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
-#define AXIS1_STEP_GOTO 1            // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
-#define AXIS2_MODE (3|TMC_STEALTHCHOP|TMC_LOWPWR) // programs the Dec/Alt uStep mode M0/M1/M2, optional and default _OFF.
-#define AXIS2_MODE_GOTO 1          // programs the Dec/Alt uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
-#define AXIS2_STEP_GOTO 1            // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
-#define MODE_SWITCH_BEFORE_SLEW_SPI  // _ON (or _SPI) for _MODE and _MODE_GOTO settings to start/stop just before/after the slew, otherwise they are active during the slew at <128uS/step speeds
-                                     // _SPI as above but uses SPI (on M0/M1/M2/Aux) to do the switching (TMC2130.)  Uses Pins 18 (Aux1) and 5 (Aux2,) choose only one feature on Aux1/2.
+#define AXIS1_MODE 4                // programs the RA/Az uStep mode M0/M1/M2, optional and default _OFF.
+#define AXIS1_MODE_GOTO 8           // programs the RA/Az uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
+#define AXIS1_STEP_GOTO 1           // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
+#define AXIS2_MODE 4                // programs the Dec/Alt uStep mode M0/M1/M2, optional and default _OFF.
+#define AXIS2_MODE_GOTO 8           // programs the Dec/Alt uStep mode M0/M1/M2, used during gotos, optional and default _OFF.
+#define AXIS2_STEP_GOTO 1           // 1=goto mode is same as normal mode: for example if normal tracking mode is 32x and goto is 8x this would be 4
+#define MODE_SWITCH_BEFORE_SLEW_ON  // _ON (or _SPI) for _MODE and _MODE_GOTO settings to start/stop just before/after the slew, otherwise they are active during the slew at <128uS/step speeds
+                                    // _SPI as above but uses SPI (on M0/M1/M2/Aux) to do the switching (TMC2130.)  Uses Pins 18 (Aux1) and 5 (Aux2,) choose only one feature on Aux1/2.
 
 // if the decay/micro-step mode switch happens before/after a slew, inserts a 3ms delay before the motors take a step.
 #define MODE_SWITCH_SLEEP_OFF
