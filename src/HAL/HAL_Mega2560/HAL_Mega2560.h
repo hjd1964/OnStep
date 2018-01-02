dec3a614253ba3ef548e219255035bf7b530edfa
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2018-01-02 14:17:01 -0500

    Cleanup of platform defines from Astro.ino, and pushing them into HAL.

--- src/HAL/HAL_Mega2560/HAL_Mega2560.h
+++ src/HAL/HAL_Mega2560/HAL_Mega2560.h
@@ -1,5 +1,8 @@
 #include <EEPROM.h>
 
+// Distance in arc-min ahead of and behind the current Equ position, used for rate calculation
+#define RefractionRateRange 30
+
 // Fast port writing help
 #define CLR(x,y) (x&=(~(1<<y)))
 #define SET(x,y) (x|=(1<<y))
