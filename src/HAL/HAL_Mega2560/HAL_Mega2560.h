2fe6b331abd7c9786a76cfdd0f308c99de1ef680
Howard Dutton <hjd1964@gmail.com>
2018-01-02 18:25:04 -0500

    Changed to a less cryptic #define

--- src/HAL/HAL_Mega2560/HAL_Mega2560.h
+++ src/HAL/HAL_Mega2560/HAL_Mega2560.h
@@ -1,10 +1,9 @@
 #include <EEPROM.h>
 
-// Distance in arc-min ahead of and behind the current Equ position, used for rate calculation
-#define RefractionRateRange 30
+// This platform doesn't support true double precision math
+#define NoDoublePrecision
 
 // Fast port writing help
 #define CLR(x,y) (x&=(~(1<<y)))
 #define SET(x,y) (x|=(1<<y))
 #define TGL(x,y) (x^=(1<<y))
-
--- src/HAL/HAL_Teensy_3/HAL_Teensy_3.h
+++ src/HAL/HAL_Teensy_3/HAL_Teensy_3.h
@@ -2,9 +2,6 @@
 
 #include <EEPROM.h>
 
-// Distance in arc-min ahead of and behind the current Equ position, used for rate calculation
-#define RefractionRateRange 10
-
 // Fast port writing help
 #define CLR(x,y) (x&=(~(1<<y)))
 #define SET(x,y) (x|=(1<<y))
