d214fc01b8c56223eef88fc0d6d0722e3601feec
Howard Dutton <hjd1964@gmail.com>
2018-01-06 15:51:15 -0500

    Various SDS fixes

--- Constants.h
+++ Constants.h
@@ -1,30 +1,41 @@
 // -----------------------------------------------------------------------------------
 // Constants
 
+// some defines to help with configuration
+#define TMC_LOWPWR      64
+#define TMC_STEALTHCHOP 32
+#define TMC_NINTPOL     16
+#define SYNC_ANYWHERE_ON
+
 // simplified stepper driver mode setup
 #ifdef AXIS1_DRIVER_MODEL
 
-// Stepper Driver Models
-#define A4988   1
-#define DRV8825 2
-#define LV8729  3
-#define RAPS128 3
-#define TMC2130 4
-#define TMC2208 5
+  // Stepper Driver Models
+  #define A4988   1
+  #define DRV8825 2
+  #define LV8729  3
+  #define RAPS128 3
+  #define TMC2208 5
 
-// Length of each stepper's table
-#define LEN_A4988   5
-#define LEN_DRV8825 6
-#define LEN_LV8729  8
-#define LEN_TMC2208 4
-#define LEN_TMC2130 9
+  #define TMC2130 100
+  #define TMC2130_QUIET 101  // these variations don't appear at run-time and are changed to "TMC2130"
+  #define TMC2130_VQUIET 102
+  #define TMC2130_QUIET_LOWPWR 103
+  #define TMC2130_VQUIET_LOWPWR 104
   
-// The various microsteps for different driver models, with the bit modes for each
-unsigned int StepsA4988  [LEN_A4988]  [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,7} };
-unsigned int StepsDRV8825[LEN_DRV8825][2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5} };
-unsigned int StepsLV8729 [LEN_LV8729] [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5}, {64,6}, {128,7} };
-unsigned int StepsTMC2208[LEN_TMC2208][2] = {        {2,0}, {4,1}, {8,2}, {16,3} };
-unsigned int StepsTMC2130[LEN_TMC2130][2] = { {1,8}, {2,7}, {4,6}, {8,5}, {16,4}, {32,3}, {64,2}, {128,1}, {256,0} };
+  // Length of each stepper's table
+  #define LEN_A4988   5
+  #define LEN_DRV8825 6
+  #define LEN_LV8729  8
+  #define LEN_TMC2208 4
+  #define LEN_TMC2130 9
+    
+  // The various microsteps for different driver models, with the bit modes for each
+  unsigned int StepsA4988  [LEN_A4988]  [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,7} };
+  unsigned int StepsDRV8825[LEN_DRV8825][2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5} };
+  unsigned int StepsLV8729 [LEN_LV8729] [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5}, {64,6}, {128,7} };
+  unsigned int StepsTMC2208[LEN_TMC2208][2] = {        {2,0}, {4,1}, {8,2}, {16,3} };
+  unsigned int StepsTMC2130[LEN_TMC2130][2] = { {1,8}, {2,7}, {4,6}, {8,5}, {16,4}, {32,3}, {64,2}, {128,1}, {256,0} };
 
 #endif
 
--- Validate.h
+++ Validate.h
@@ -1,6 +1,8 @@
 // -----------------------------------------------------------------------------------
 // Validate current configuration
 
+#include "Constants.h"
+
 #if !defined(Classic_ON) && !defined(MiniPCB_ON) && !defined(MaxPCB_ON) && !defined(TM4C_ON) && !defined(Ramps14_ON) && !defined(Mega2560Alt_ON) && !defined(STM32_ON)
   #error "Choose ONE Config.xxx.h file and enable it for use by turning it _ON."
 #endif
@@ -14,9 +16,9 @@
 #endif
 
 #if defined(MaxPCB) || defined(MiniPCB)
-#if defined(RETICULE_LED_PINS) && (defined(STATUS_LED_PINS_ON) || defined(STATUS_LED_PINS))
-  #error "You can't have the Illuminated Reticule and Status2 LEDs both enabled in this configuration."
-#endif
+  #if defined(RETICULE_LED_PINS) && (defined(STATUS_LED_PINS_ON) || defined(STATUS_LED_PINS))
+    #error "You can't have the Illuminated Reticule and Status2 LEDs both enabled in this configuration."
+  #endif
 #endif
 
 // -----------------------------------------------------------------------------------
@@ -33,138 +35,133 @@
 // figure out how many align star are allowed for the configuration
 #ifndef ALIGN_GOTOASSIST_ON
   #if defined(MOUNT_TYPE_GEM)
-  #define MAX_NUM_ALIGN_STARS '3'
+    #define MAX_NUM_ALIGN_STARS '3'
   #elif defined(MOUNT_TYPE_FORK)
-  #define MAX_NUM_ALIGN_STARS '3'
+    #define MAX_NUM_ALIGN_STARS '3'
   #elif defined(MOUNT_TYPE_FORK_ALT)
-  #define MAX_NUM_ALIGN_STARS '1'
+    #define MAX_NUM_ALIGN_STARS '1'
   #elif defined(MOUNT_TYPE_ALTAZM)
-  #define MAX_NUM_ALIGN_STARS '3'
+    #define MAX_NUM_ALIGN_STARS '3'
   #endif
 #else
   #if defined(MOUNT_TYPE_GEM)
-  #define MAX_NUM_ALIGN_STARS '6'
+    #define MAX_NUM_ALIGN_STARS '6'
   #elif defined(MOUNT_TYPE_FORK)
-  #define MAX_NUM_ALIGN_STARS '6'
+    #define MAX_NUM_ALIGN_STARS '6'
   #elif defined(MOUNT_TYPE_FORK_ALT)
-  #define MAX_NUM_ALIGN_STARS '1'
+    #define MAX_NUM_ALIGN_STARS '1'
   #elif defined(MOUNT_TYPE_ALTAZM)
-  #define MAX_NUM_ALIGN_STARS '3'
+    #define MAX_NUM_ALIGN_STARS '3'
   #endif
 #endif
 
 // misc. configuration #defines to correct for backwards compatability etc.
 #ifdef SEPERATE_PULSE_GUIDE_RATE_ON
-#define SEPARATE_PULSE_GUIDE_RATE_ON
+  #define SEPARATE_PULSE_GUIDE_RATE_ON
 #endif
 #ifndef GUIDE_TIME_LIMIT
-#define GUIDE_TIME_LIMIT 0
+  #define GUIDE_TIME_LIMIT 0
 #endif
 #ifndef MaxRot
-#define MaxRot MaxAxis3
+  #define MaxRot MaxAxis3
 #endif
 #ifndef MinRot
-#define MinRot MinAxis3
-#endif
-#ifndef AXIS1_STEP_GOTO
-#define AXIS1_STEP_GOTO 1
-#endif
-#ifndef AXIS2_STEP_GOTO
-#define AXIS2_STEP_GOTO 1
+  #define MinRot MinAxis3
 #endif
 
 // -----------------------------------------------------------------------------------
 // stepper driver mode setup
 
-#if (defined(AXIS1_DRIVER_MODEL) && (!defined(AXIS2_DRIVER_MODEL)) || (defined(AXIS2_DRIVER_MODEL) && (!defined(AXIS1_DRIVER_MODEL))
+#if (defined(AXIS1_DRIVER_MODEL) && !defined(AXIS2_DRIVER_MODEL)) || (defined(AXIS2_DRIVER_MODEL) && !defined(AXIS1_DRIVER_MODEL))
   #error "AXISn_DRIVER_MODEL; If using the simplified driver mode setup you must use it for both AXIS1 and AXIS2."
 #endif
 
 #if defined(AXIS1_DRIVER_MODEL)
   // attempting to use the simplified stepper driver setup
 
+
   // special SPI modes for TMC2130
   #if AXIS1_DRIVER_MODEL == TMC2130_QUIET
     #undef AXIS1_DRIVER_MODEL
-    #define AXIS1_DRIVER_MODE TMC2130
+    #define AXIS1_DRIVER_MODEL TMC2130
     #define TMC_AXIS1_MODE TMC_STEALTHCHOP
     #define TMC_AXIS1_MODE_GOTO 0
   #elif AXIS1_DRIVER_MODEL == TMC2130_QUIET_LOWPWR
     #undef AXIS1_DRIVER_MODEL
-    #define AXIS1_DRIVER_MODE TMC2130
+    #define AXIS1_DRIVER_MODEL TMC2130
     #define TMC_AXIS1_MODE TMC_STEALTHCHOP|TMC_LOWPWR
     #define TMC_AXIS1_MODE_GOTO 0
   #elif AXIS1_DRIVER_MODEL == TMC2130_VQUIET
     #undef AXIS1_DRIVER_MODEL
-    #define AXIS1_DRIVER_MODE TMC2130
+    #define AXIS1_DRIVER_MODEL TMC2130
     #define TMC_AXIS1_MODE TMC_STEALTHCHOP
     #define TMC_AXIS1_MODE_GOTO TMC_STEALTHCHOP
   #elif AXIS1_DRIVER_MODEL == TMC2130_VQUIET_LOWPWR
     #undef AXIS1_DRIVER_MODEL
-    #define AXIS1_DRIVER_MODE TMC2130
+    #define AXIS1_DRIVER_MODEL TMC2130
     #define TMC_AXIS1_MODE TMC_STEALTHCHOP|TMC_LOWPWR
     #define TMC_AXIS1_MODE_GOTO TMC_STEALTHCHOP
-  else
+  #else
     #define TMC_AXIS1_MODE 0
     #define TMC_AXIS1_MODE_GOTO 0
   #endif
 
   #if AXIS2_DRIVER_MODEL == TMC2130_QUIET
     #undef AXIS2_DRIVER_MODEL
-    #define AXIS2_DRIVER_MODE TMC2130
+    #define AXIS2_DRIVER_MODEL TMC2130
     #define TMC_AXIS2_MODE TMC_STEALTHCHOP
     #define TMC_AXIS2_MODE_GOTO 0
   #elif AXIS2_DRIVER_MODEL == TMC2130_QUIET_LOWPWR
     #undef AXIS2_DRIVER_MODEL
-    #define AXIS2_DRIVER_MODE TMC2130
+    #define AXIS2_DRIVER_MODEL TMC2130
     #define TMC_AXIS2_MODE TMC_STEALTHCHOP|TMC_LOWPWR
     #define TMC_AXIS2_MODE_GOTO 0
   #elif AXIS2_DRIVER_MODEL == TMC2130_VQUIET
     #undef AXIS2_DRIVER_MODEL
-    #define AXIS2_DRIVER_MODE TMC2130
+    #define AXIS2_DRIVER_MODEL TMC2130
     #define TMC_AXIS2_MODE TMC_STEALTHCHOP
     #define TMC_AXIS2_MODE_GOTO TMC_STEALTHCHOP
   #elif AXIS2_DRIVER_MODEL == TMC2130_VQUIET_LOWPWR
     #undef AXIS2_DRIVER_MODEL
-    #define AXIS2_DRIVER_MODE TMC2130
+    #define AXIS2_DRIVER_MODEL TMC2130
     #define TMC_AXIS2_MODE TMC_STEALTHCHOP|TMC_LOWPWR
     #define TMC_AXIS2_MODE_GOTO TMC_STEALTHCHOP
-  else
+  #else
     #define TMC_AXIS2_MODE 0
     #define TMC_AXIS2_MODE_GOTO 0
   #endif
 
   // tracking mode
   #ifndef AXIS1_MICROSTEPS
-    #error "AXIS1_MICROSTEPS must be defined in your Config.xxx.h file if using AXIS1_DRIVER_MODEL!"
+    #error "Configuration error: AXIS1_MICROSTEPS must be defined in your Config.xxx.h file if using AXIS1_DRIVER_MODEL!"
   #endif
 
   #if AXIS1_DRIVER_MODEL == A4988
     #if AXIS1_MICROSTEPS!=1 && AXIS1_MICROSTEPS!=2 && AXIS1_MICROSTEPS!=4 && AXIS1_MICROSTEPS!=8 && AXIS1_MICROSTEPS!=16
-      #error "AXIS1_MICROSTEPS; A4988 invalid micro-step mode, use: 16,8,4,2,or 1"
+      #error "Configuration error: AXIS1_MICROSTEPS; A4988 invalid micro-step mode, use: 16,8,4,2,or 1"
     #endif
   #elif AXIS1_DRIVER_MODEL == DRV8825
     #if AXIS1_MICROSTEPS!=1 && AXIS1_MICROSTEPS!=2 && AXIS1_MICROSTEPS!=4 && AXIS1_MICROSTEPS!=8 && AXIS1_MICROSTEPS!=16 && AXIS1_MICROSTEPS!=32
-      #error "AXIS1_MICROSTEPS; DRV8825 invalid micro-step mode, use: 32,16,8,4,2,or 1"
+      #error "Configuration error: AXIS1_MICROSTEPS; DRV8825 invalid micro-step mode, use: 32,16,8,4,2,or 1"
     #endif
   #elif AXIS1_DRIVER_MODEL == LV8729 || AXIS1_DRIVER_MODEL == RAPS128
     #if AXIS1_MICROSTEPS!=1 && AXIS1_MICROSTEPS!=2 && AXIS1_MICROSTEPS!=4 && AXIS1_MICROSTEPS!=8 && AXIS1_MICROSTEPS!=16 && AXIS1_MICROSTEPS!=32 && AXIS1_MICROSTEPS!=64 && AXIS1_MICROSTEPS!=128
-      #error "AXIS1_MICROSTEPS; LV8729/RAPS128 invalid micro-step mode, use: 128,64,32,16,8,4,2,or 1"
+      #error "Configuration error: AXIS1_MICROSTEPS; LV8729/RAPS128 invalid micro-step mode, use: 128,64,32,16,8,4,2,or 1"
     #endif
   #elif AXIS1_DRIVER_MODEL == TMC2100
     #if AXIS1_MICROSTEPS!=_OFF
-      #error "AXIS1_MICROSTEPS; TMC2100 invalid micro-step mode, use: _OFF"
+      #error "Configuration error: AXIS1_MICROSTEPS; TMC2100 invalid micro-step mode, use: _OFF"
     #endif
   #elif AXIS1_DRIVER_MODEL == TMC2130
     #if AXIS1_MICROSTEPS!=1 && AXIS1_MICROSTEPS!=2 && AXIS1_MICROSTEPS!=4 && AXIS1_MICROSTEPS!=8 && AXIS1_MICROSTEPS!=16 && AXIS1_MICROSTEPS!=32 && AXIS1_MICROSTEPS!=64 && AXIS1_MICROSTEPS!=128 && AXIS1_MICROSTEPS!=256
-      #error "AXIS1_MICROSTEPS; TMC2130 invalid micro-step mode, use: 256,128,64,32,16,8,4,2,or 1"
+      #error "Configuration error: AXIS1_MICROSTEPS; TMC2130 invalid micro-step mode, use: 256,128,64,32,16,8,4,2,or 1"
     #endif
   #elif AXIS1_DRIVER_MODEL == TMC2208
     #if AXIS1_MICROSTEPS!=2 && AXIS1_MICROSTEPS!=4 && AXIS1_MICROSTEPS!=8 && AXIS1_MICROSTEPS!=16
-      #error "AXIS1_MICROSTEPS; TMC2208 invalid micro-step mode, use: 16,8,4,or 2"
+      #error "Configuration error: AXIS1_MICROSTEPS; TMC2208 invalid micro-step mode, use: 16,8,4,or 2"
     #endif
   #else
-    #error "No stepper driver model configured for Axis1!"
+    #error "Configuration error: Unrecognized stepper driver model for Axis1 !"
   #endif
 
   #ifndef AXIS2_MICROSTEPS
@@ -198,7 +195,7 @@
       #error "Configuration error: AXIS2_MICROSTEPS; TMC2208 invalid micro-step mode, use: 16,8,4,or 2"
     #endif
   #else
-    #error "No stepper driver model configured for axis 1 !"
+    #error "Configuration error: Unrecognized stepper driver model for Axis2 !"
   #endif
 
   // goto mode
@@ -269,11 +266,12 @@
   #endif
 
   #if defined(MODE_SWITCH_BEFORE_SLEW_ON) || defined(MODE_SWITCH_BEFORE_SLEW_SPI)
-  #if !defined(AXIS1_MICROSTEPS) || !defined(AXIS1_MICROSTEPS_GOTO)
-    #error "Configuration error: AXIS1_MICROSTEPS and AXIS1_MICROSTEPS_GOTO must be set to a valid value."
-  #endif
-  #if !defined(AXIS2_MICROSTEPS) || !defined(AXIS2_MICROSTEPS_GOTO)
-    #error "Configuration error: AXIS2_MICROSTEPS and AXIS2_MICROSTEPS_GOTO must be set to a valid value."
+    #if !defined(AXIS1_MICROSTEPS) || !defined(AXIS1_MICROSTEPS_GOTO)
+      #error "Configuration error: AXIS1_MICROSTEPS and AXIS1_MICROSTEPS_GOTO must be set to a valid value."
+    #endif
+    #if !defined(AXIS2_MICROSTEPS) || !defined(AXIS2_MICROSTEPS_GOTO)
+      #error "Configuration error: AXIS2_MICROSTEPS and AXIS2_MICROSTEPS_GOTO must be set to a valid value."
+    #endif
   #endif
 
 #else
@@ -288,6 +286,14 @@
     #endif
   #endif
 
+#endif
+
+// finally, if AXISn_STEP_GOTO isn't defined, do so
+#ifndef AXIS1_STEP_GOTO
+  #define AXIS1_STEP_GOTO 1
+#endif
+#ifndef AXIS2_STEP_GOTO
+  #define AXIS2_STEP_GOTO 1
 #endif
 
 
