4d6067c4fa2f32dba7cba8b641530e44b6a21e1d
Howard Dutton <hjd1964@gmail.com>
2018-01-07 11:08:41 -0500

    Refactoring

--- Constants.h
+++ Constants.h
@@ -20,7 +20,7 @@
 #define TMC2130_VQUIET 102
 #define TMC2130_QUIET_LOWPWR 103
 #define TMC2130_VQUIET_LOWPWR 104
-  
+
 // Length of each stepper's table
 #define LEN_A4988   5
 #define LEN_DRV8825 6
@@ -28,7 +28,7 @@
 #define LEN_TMC2100 4
 #define LEN_TMC2208 4
 #define LEN_TMC2130 9
-    
+   
 // The various microsteps for different driver models, with the bit modes for each
 unsigned int StepsA4988  [LEN_A4988]  [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,7} };
 unsigned int StepsDRV8825[LEN_DRV8825][2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5} };
@@ -36,3 +36,4 @@ unsigned int StepsLV8729 [LEN_LV8729] [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}
 unsigned int StepsTMC2208[LEN_TMC2208][2] = {        {2,0}, {4,1}, {8,2}, {16,3} };
 unsigned int StepsTMC2100[LEN_TMC2100][2] = { {1,0}, {2,1}, {4,2}, {16,3} };
 unsigned int StepsTMC2130[LEN_TMC2130][2] = { {1,8}, {2,7}, {4,6}, {8,5}, {16,4}, {32,3}, {64,2}, {128,1}, {256,0} };
+
