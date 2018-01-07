95d0f4c94b2606600bf47f19bfb48aac8b42b01d
Howard Dutton <hjd1964@gmail.com>
2018-01-06 19:13:09 -0500

    SDS Added TMC2100

--- Constants.h
+++ Constants.h
@@ -15,6 +15,7 @@
   #define DRV8825 2
   #define LV8729  3
   #define RAPS128 3
+  #define TMC2100 4
   #define TMC2208 5
 
   #define TMC2130 100
@@ -27,6 +28,7 @@
   #define LEN_A4988   5
   #define LEN_DRV8825 6
   #define LEN_LV8729  8
+  #define LEN_TMC2100 4
   #define LEN_TMC2208 4
   #define LEN_TMC2130 9
     
@@ -34,6 +36,7 @@
   unsigned int StepsA4988  [LEN_A4988]  [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,7} };
   unsigned int StepsDRV8825[LEN_DRV8825][2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5} };
   unsigned int StepsLV8729 [LEN_LV8729] [2] = { {1,0}, {2,1}, {4,2}, {8,3}, {16,4}, {32,5}, {64,6}, {128,7} };
+  unsigned int StepsTMC2100[LEN_TMC2100][2] = { {1,0}, {2,1}, {4,2}, {16,3} };
   unsigned int StepsTMC2208[LEN_TMC2208][2] = {        {2,0}, {4,1}, {8,2}, {16,3} };
   unsigned int StepsTMC2130[LEN_TMC2130][2] = { {1,8}, {2,7}, {4,6}, {8,5}, {16,4}, {32,3}, {64,2}, {128,1}, {256,0} };
 
