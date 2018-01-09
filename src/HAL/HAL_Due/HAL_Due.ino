8c6c4fde17ba27cf2211068a3faba1588553aa2c
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2018-01-08 20:11:09 -0500

    Better comment inside placeholder .ino files.

--- src/HAL/HAL_Due/HAL_Due.ino
+++ src/HAL/HAL_Due/HAL_Due.ino
@@ -1,1 +1,4 @@
-// Dummy file
+// Placeholder file
+// Nothing to see here ...
+//
+// This file is only present so the Arduino IDE can edit the .h file(s)
--- src/HAL/HAL_STM32F1/HAL_STM32F1.ino
+++ src/HAL/HAL_STM32F1/HAL_STM32F1.ino
@@ -1,1 +1,5 @@
-// Dummy file
+// Placeholder file
+// Nothing to see here ...
+//
+// This file is only present so the Arduino IDE can edit the .h file(s)
+
