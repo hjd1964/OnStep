7055cbc66ce4bf8d17733692e7a182f905dff711
Howard Dutton <hjd1964@gmail.com>
2018-03-22 13:56:27 -0400

    Upped revision

--- OnStep.ino
+++ OnStep.ino
@@ -41,7 +41,7 @@
 #define FirmwareDate          __DATE__
 #define FirmwareVersionMajor  1
 #define FirmwareVersionMinor  4
-#define FirmwareVersionPatch  "a"     // for example major.minor patch: 1.3c
+#define FirmwareVersionPatch  "b"     // for example major.minor patch: 1.3c
 #define FirmwareVersionConfig 2       // internal, for tracking configuration file changes
 #define FirmwareName          "On-Step"
 #define FirmwareTime          __TIME__
