955e3b88ea02d435c4ae059c051db1d8f7620ce2
Howard Dutton <hjd1964@gmail.com>
2018-04-15 13:19:10 -0400

    LED should default to off

--- src/addons/WiFi-Bluetooth/Config.h
+++ src/addons/WiFi-Bluetooth/Config.h
@@ -8,7 +8,7 @@
 
 #define SERIAL_BAUD 57600         // Default=57600, use 19200 for talking to OnStep on a Mega2560
 
-#define LED_PIN D4                // Default=_OFF, Flashes LED when trying to connect to OnStep, use LED_PIN D4 for WeMos D1 Mini
+#define LED_PIN_OFF               // Default=_OFF, Flashes LED when trying to connect to OnStep, use LED_PIN D4 for WeMos D1 Mini
 
 #define SERIAL_SWAP_OFF           // Default=_OFF, _ON to swap ESP8266 Serial default location GPIO 1(TX)/3(RX) to 15(TX)/13(RX) useful for WeMos D1 Mini, for example
 
