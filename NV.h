c8db769f9e27f72db29b4c4943c5b6c65238a697
Howard Dutton <hjd1964@gmail.com>
2018-05-01 20:05:47 -0400

    Refactored/simplified EEPROM code

--- NV.h
+++ NV.h
@@ -10,57 +10,57 @@ class nvs {
       return EEPROM.read(i);
     }
 
-    void write(int i, byte j) {
+    void update(int i, byte j) {
       EEPROM.update(i,j);
     }
 
-    void update(int i, byte j) {
-      EEPROM.update(i,j);
+    void write(int i, byte j) {
+      update(i,j);
     }
 
     // write int numbers into EEPROM at position i (2 bytes)
     void writeInt(int i,int j) {
       uint8_t *k = (uint8_t*)&j;
-      EEPROM.update(i+0,*k); k++;
-      EEPROM.update(i+1,*k);
+      update(i+0,*k); k++;
+      update(i+1,*k);
     }
     
     // read int numbers from EEPROM at position i (2 bytes)
     int readInt(int i) {
       uint16_t j;
       uint8_t *k = (uint8_t*)&j;
-      *k=EEPROM.read(i+0); k++;
-      *k=EEPROM.read(i+1);
+      *k=read(i+0); k++;
+      *k=read(i+1);
       return j;
     }
     
     // write 4 byte variable into EEPROM at position i (4 bytes)
     void writeQuad(int i,byte *v) {
-      EEPROM.update(i+0,*v); v++;
-      EEPROM.update(i+1,*v); v++;
-      EEPROM.update(i+2,*v); v++;
-      EEPROM.update(i+3,*v);
+      update(i+0,*v); v++;
+      update(i+1,*v); v++;
+      update(i+2,*v); v++;
+      update(i+3,*v);
     }
     
     // read 4 byte variable from EEPROM at position i (4 bytes)
     void readQuad(int i,byte *v) {
-      *v=EEPROM.read(i+0); v++;
-      *v=EEPROM.read(i+1); v++;
-      *v=EEPROM.read(i+2); v++;
-      *v=EEPROM.read(i+3);  
+      *v=read(i+0); v++;
+      *v=read(i+1); v++;
+      *v=read(i+2); v++;
+      *v=read(i+3);  
     }
     
     // write String into EEPROM at position i (16 bytes)
     void writeString(int i,char l[]) {
       for (int l1=0; l1<16; l1++) {
-        EEPROM.update(i+l1,*l); l++;
+        update(i+l1,*l); l++;
       }
     }
     
     // read String from EEPROM at position i (16 bytes)
     void readString(int i, char l[]) {
       for (int l1=0; l1<16; l1++) {
-        *l=EEPROM.read(i+l1); l++;
+        *l=read(i+l1); l++;
       }
     }
     
