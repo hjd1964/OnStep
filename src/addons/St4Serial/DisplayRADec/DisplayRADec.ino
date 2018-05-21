0a3ea6e53e8e9d8a45f6a4d6087a5e02bb21c183
Howard Dutton <hjd1964@gmail.com>
2018-05-21 13:52:14 -0400

    SHC pins match EasyEDA design

--- src/addons/St4Serial/DisplayRADec/DisplayRADec.ino
+++ src/addons/St4Serial/DisplayRADec/DisplayRADec.ino
@@ -3,10 +3,10 @@
 // use this if running on Teensy3.x (or Teensy LC) hardware, otherwise comment out and assumes Atmel AVR
 #define __ARM_Teensy3__
 
-#define ST4RAw 21 // ST4 RA- West,  send data to OnStep
-#define ST4DEs 22 // ST4 DE- South, clock input to ISR
-#define ST4DEn 23 // ST4 DE+ North, recv data from OnStep
-#define ST4RAe 17 // ST4 RA+ East,  always 12.5 Hz square wave on this pin
+#define ST4RAw 2 // ST4 RA- West,  send data to OnStep
+#define ST4DEs 3 // ST4 DE- South, clock input to ISR
+#define ST4DEn 4 // ST4 DE+ North, recv data from OnStep
+#define ST4RAe 5 // ST4 RA+ East,  always 12.5 Hz square wave on this pin
 
 #include "St4SerialSlave.h"
 #include <U8g2lib.h>  // use Arduino->Sketch->Include Library->Manage Libraries then search for "u8g2" and install
--- src/addons/St4Serial/SmartHandController/Config.h
+++ src/addons/St4Serial/SmartHandController/Config.h
@@ -6,16 +6,16 @@
 
 // the serial interface to/from OnStep (over ST4 pins)
 #define Ser SerialST4             // you could also, for example, use the Teensy3.2's Serial1 interface "Ser Serial1"
-#define ST4RAw 21                 // ST4 RA- West,  send data to OnStep
-#define ST4DEs 22                 // ST4 DE- South, clock input to ISR
-#define ST4DEn 23                 // ST4 DE+ North, recv data from OnStep
-#define ST4RAe 17                 // ST4 RA+ East,  always 25 Hz square wave on this pin
+#define ST4RAw 2                 // ST4 RA- West,  send data to OnStep
+#define ST4DEs 3                 // ST4 DE- South, clock input to ISR
+#define ST4DEn 4                 // ST4 DE+ North, recv data from OnStep
+#define ST4RAe 5                 // ST4 RA+ East,  always 25 Hz square wave on this pin
 #include "St4SerialSlave.h"
 
 // the hand controller buttons
-OneButton buttons[7] = { OneButton(2, false),
-                        OneButton(3, false), OneButton(4, false),OneButton(5, false),OneButton(6, false),
-                        OneButton(7, true), OneButton(8, true) };
+OneButton buttons[7] = { OneButton(21, false),
+                        OneButton(15, false), OneButton(16, false),OneButton(17, false),OneButton(20, false),
+                        OneButton(22, true), OneButton(23, true) };
 
 // choose a 128x64 display supported by U8G2lib (if not listed below there are many many others in u8g2 library example Sketches)
 U8G2_SSD1306_128X64_NONAME_F_SW_I2C display(U8G2_R0, /* clock=*/ SCL, /* data=*/ SDA, /* reset=*/ U8X8_PIN_NONE);   // All Boards without Reset of the Display
