32e9f4b2b7191dd2a35e3d07e8e362e58da58cd1
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2018-05-26 11:30:19 -0400

    Default debug to off

--- OnStep.ino
+++ OnStep.ino
@@ -64,7 +64,7 @@
 #include "src/lib/St4SerialMaster.h"
 
 // Enable debugging messages on DebugSer -------------------------------------------------------------
-#define DEBUG_ON              // default=_OFF, use "DEBUG_ON" to activate
+#define DEBUG_OFF              // default=_OFF, use "DEBUG_ON" to activate
 #define DebugSer SerialA       // default=Serial, or Serial1 for example (always 9600 baud)
 
 // Helper macros for debugging, with less typing
