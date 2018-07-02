329cb88dbdace4bdd69033f47781e5ee4eea9ac8
Howard Dutton <hjd1964@gmail.com>
2018-07-02 10:20:59 -0400

    Dynamic guide and abort slew rates
    So rates approximately match distance based accleleration used during
    gotos

--- Globals.h
+++ Globals.h
@@ -236,7 +236,7 @@ unsigned long baudRate[10] = {115200,56700,38400,28800,19200,14400,9600,4800,240
 #define RateToDegPerSec  (1000000.0/(double)StepsPerDegreeAxis1)
 #define RateToASPerSec   (RateToDegPerSec*3600.0)
 #define RateToXPerSec    (RateToASPerSec/15.0)
-double  slewRateX     =  (RateToXPerSec/MaxRate); // double since exponential factor slows by about 1/2
+double  slewRateX     =  (RateToXPerSec/MaxRate)*2.5; // 5x for exponential factor average rate and / 2x for default MaxRate
 double  accXPerSec    =  (slewRateX/DegreesForAcceleration);
 double  guideRates[10]={3.75,7.5,15,30,60,120,360,720,(RateToASPerSec/MaxRate)/2.0,RateToASPerSec/MaxRate};
 //                      .25X .5x 1x 2x 4x  8x 24x 48x       half-MaxRate                   MaxRate
