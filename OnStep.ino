3821137347a7a2407d0d928fb302d0028393c9fa
Howard Dutton <hjd1964@gmail.com>
2018-07-18 21:25:24 -0400

    Upped patch level

--- OnStep.ino
+++ OnStep.ino
@@ -41,7 +41,7 @@
 #define FirmwareDate          __DATE__
 #define FirmwareVersionMajor  1
 #define FirmwareVersionMinor  11
-#define FirmwareVersionPatch  "b"     // for example major.minor patch: 1.3c
+#define FirmwareVersionPatch  "c"     // for example major.minor patch: 1.3c
 #define FirmwareVersionConfig 2       // internal, for tracking configuration file changes
 #define FirmwareName          "On-Step"
 #define FirmwareTime          __TIME__
