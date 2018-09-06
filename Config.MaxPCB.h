0313bcf78842e492867418e5f93523405971a9fd
Howard Dutton <hjd1964@gmail.com>
2018-09-06 19:02:47 -0400

    Added ability to compile for the MaxESP

--- Config.MaxPCB.h
+++ Config.MaxPCB.h
@@ -221,6 +221,10 @@
 
 // -------------------------------------------------------------------------------------------------------------------------
 #define FileVersionConfig 2
-#include "src/pinmaps/Pins.MaxPCB.h"
+#ifdef ESP32
+  #include "src/pinmaps/Pins.MaxESP.h"
+#else
+  #include "src/pinmaps/Pins.MaxPCB.h"
+#endif
 #endif
 
