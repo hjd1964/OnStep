18d932018ae1771f28c83f2a614da23ac50506a4
Howard Dutton <hjd1964@gmail.com>
2018-10-12 10:32:31 -0400

    Checksum protocol sequence fix

--- src/lib/Command.h
+++ src/lib/Command.h
@@ -31,17 +31,17 @@ class cb {
         if (checksum) {
           byte len=strlen(cb)-1;
 
-          // Minimum length for a valid command is 5 ';SCC#'
+          // Minimum length for a valid command is 5 ';CCS#'
           if (len<5) {
             flush(); cb[0]=':'; cb[1]=(char)6; cb[2]='0'; cb[3]='#'; cb[4]=0; cbp=4; 
             return true; 
           }
           
-          // checksum the data, for example ";111111SCC#".  I don't include the command frame in the checksum.  The error response is a checksumed string "CK_FAILS#" to request re-transmit.
+          // checksum the data, for example ";111111CCS#".  I don't include the command frame in the checksum.  The error response is a checksumed string "CK_FAILS#" to request re-transmit.
           byte cks=0; for (int cksCount0=1; cksCount0<len-3; cksCount0++) {  cks+=cb[cksCount0]; }
           char chkSum[3]; sprintf(chkSum,"%02X",cks);
-          seq=cb[len-3];
-          if (!((chkSum[0]==cb[len-2]) && (chkSum[1]==cb[len-1]))) { 
+          seq=cb[len-1];
+          if (!((chkSum[0]==cb[len-3]) && (chkSum[1]==cb[len-2]))) { 
             flush(); cb[0]=':'; cb[1]=(char)6; cb[2]='0'; cb[3]='#'; cb[4]=0; cbp=4; 
             return true;
           }
