4419cdd6faa2d2a851124855d6f238089901307b
Khalid Baheyeldin <29777018+kbahey@users.noreply.github.com>
2018-11-17 10:27:24 -0500

    Changed 2L to 4L, and 40 seconds to 120 seconds, for better tracking with high Steps Per Degrees.

--- Timer.ino
+++ Timer.ino
@@ -24,6 +24,12 @@ volatile bool axis2Powered = true;
 //--------------------------------------------------------------------------------------------------
 // Set hardware timer rates
 
+#if defined(__ARM_STM32__)
+  #define MULT      4L
+#else
+  #define MULT   1024L
+#endif
+
 // set Timer1 master sidereal clock to interval (in microseconds*16)
 volatile long isrTimerRateAxis1=0;
 volatile long isrTimerRateAxis2=0;
@@ -49,12 +55,9 @@ void timer3SetInterval(long iv) {
   // 0.0327 * 4096 = 134.21s
   uint32_t i=iv; uint16_t t=1; while (iv>65536L) { t*=2; iv=i/t; if (t==4096) { iv=65535L; break; } }
   cli(); nextAxis1Rate=iv-1L; t3rep=t; fastAxis1=(t3rep==1); sei();
-#elif defined(__ARM_STM32__)
-  uint32_t i=iv; uint16_t t=1; while (iv>65536L*2L) { t++; iv=i/t; if (t==32) { iv=65535L*2L; break; } }
-  cli(); nextAxis1Rate=((F_COMP/1000000.0) * (iv*0.0625) * 0.5 - 1.0); t3rep=t; fastAxis1=(t3rep==1); sei();
 #else
   // 4.194 * 32 = 134.21s
-  uint32_t i=iv; uint16_t t=1; while (iv>65536L*1024L) { t++; iv=i/t; if (t==32) { iv=65535L*1024L; break; } }
+  uint32_t i=iv; uint16_t t=1; while (iv>65536L*MULT) { t++; iv=i/t; if (t==32) { iv=65535L*MULT; break; } }
   cli(); nextAxis1Rate=((F_COMP/1000000.0) * (iv*0.0625) * 0.5 - 1.0); t3rep=t; fastAxis1=(t3rep==1); sei();
 #endif
 }
@@ -72,12 +75,9 @@ void timer4SetInterval(long iv) {
   // 0.0327 * 4096 = 134.21s
   uint32_t i=iv; uint16_t t=1; while (iv>65536L) { t*=2; iv=i/t; if (t==4096) { iv=65535L; break; } }
   cli(); nextAxis2Rate=iv-1L; t4rep=t; fastAxis2=(t4rep==1); sei();
-#elif defined(__ARM_STM32__)
-  uint32_t i=iv; uint16_t t=1; while (iv>65536L*2L) { t++; iv=i/t; if (t==32) { iv=65535L*2L; break; } }
-  cli(); nextAxis2Rate=((F_COMP/1000000.0) * (iv*0.0625) * 0.5 - 1.0); t4rep=t; fastAxis2=(t4rep==1); sei();
 #else
   // 4.194 * 32 = 134.21s
-  uint32_t i=iv; uint16_t t=1; while (iv>65536L*1024L) { t++; iv=i/t; if (t==32) { iv=65535L*1024L; break; } }
+  uint32_t i=iv; uint16_t t=1; while (iv>65536L*MULT) { t++; iv=i/t; if (t==32) { iv=65535L*MULT; break; } }
   cli(); nextAxis2Rate=((F_COMP/1000000.0) * (iv*0.0625) * 0.5 - 1.0); t4rep=t; fastAxis2=(t4rep==1); sei();
 #endif
 }
@@ -419,7 +419,7 @@ void autoPowerDownAxis2() {
 #if defined(PPS_SENSE_ON) || defined(PPS_SENSE_PULLUP)
 // PPS interrupt
 void clockSync() {
-  #define NUM_SECS_TO_AVERAGE 40
+  #define NUM_SECS_TO_AVERAGE 120
   unsigned long t=micros();
   unsigned long oneS=(t-PPSlastMicroS);
   if ((oneS>1000000-20000) && (oneS<1000000+20000)) {
