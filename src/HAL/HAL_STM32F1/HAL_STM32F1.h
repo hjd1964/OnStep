bb01217c53b97fef76c9849a2ca477c6bb7bf8d2
Howard Dutton <hjd1964@gmail.com>
2019-02-26 20:04:06 -0500

    Fixed MaxRate lower limit #define for STM32

--- src/HAL/HAL_STM32F1/HAL_STM32F1.h
+++ src/HAL/HAL_STM32F1/HAL_STM32F1.h
@@ -5,7 +5,7 @@
 
 // Lower limit (fastest) step rate in uS for this platform -------------------------------------------
 // the exact model should be detected and these tailored to each, but this is a good starting point
-#define MaxRateLowerLimit 24
+#define MaxRate_LowerLimit 24
 
 #include <HardwareTimer.h>
 
