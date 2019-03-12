dbf2d8425d9dde979716071953679f12a0aab980
Howard Dutton <hjd1964@gmail.com>
2019-03-12 16:19:14 -0400

    Corrected boolean logic for guide signal debounce, thanks Dave

--- addons/St4Serial/SmartHandController/St4Aux.cpp
+++ addons/St4Serial/SmartHandController/St4Aux.cpp
@@ -18,10 +18,10 @@
     pinMode(ST4AuxDEn,INPUT);
     pinMode(ST4AuxRAe,INPUT);
   }
-  bool Ast4::guideNorth() { v=digitalRead(ST4AuxRAw); delayMicroseconds(2); v1=digitalRead(ST4AuxRAw); return ((v&v1)==LOW); }
-  bool Ast4::guideSouth() { v=digitalRead(ST4AuxDEs); delayMicroseconds(2); v1=digitalRead(ST4AuxDEs); return ((v&v1)==LOW); }
-  bool Ast4::guideEast() { v=digitalRead(ST4AuxDEn); delayMicroseconds(2); v1=digitalRead(ST4AuxDEn); return ((v&v1)==LOW); }
-  bool Ast4::guideWest() { v=digitalRead(ST4AuxRAe); delayMicroseconds(2); v1=digitalRead(ST4AuxRAe); return ((v&v1)==LOW); }
+  bool Ast4::guideNorth() { v=digitalRead(ST4AuxRAw); delayMicroseconds(2); v1=digitalRead(ST4AuxRAw); return ((v==LOW) && (v1==LOW)); }
+  bool Ast4::guideSouth() { v=digitalRead(ST4AuxDEs); delayMicroseconds(2); v1=digitalRead(ST4AuxDEs); return ((v==LOW) && (v1==LOW)); }
+  bool Ast4::guideEast()  { v=digitalRead(ST4AuxDEn); delayMicroseconds(2); v1=digitalRead(ST4AuxDEn); return ((v==LOW) && (v1==LOW)); }
+  bool Ast4::guideWest()  { v=digitalRead(ST4AuxRAe); delayMicroseconds(2); v1=digitalRead(ST4AuxRAe); return ((v==LOW) && (v1==LOW)); }
 #endif
 
 Ast4 auxST4;
