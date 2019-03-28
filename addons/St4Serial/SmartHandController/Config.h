46ee49b232b27b6fbf29df7c5f201cfa266cb67a
Howard Dutton <hjd1964@gmail.com>
2019-03-28 12:51:21 -0400

    Default to setting 24 hour time

--- addons/St4Serial/SmartHandController/Config.h
+++ addons/St4Serial/SmartHandController/Config.h
@@ -15,8 +15,8 @@
 // UTILITY_LIGHT n, where n=0 to 255 activates this feature and sets default brightness
 #define UTILITY_LIGHT_OFF
 
-// Use 24 hour format for entering time, default=false
-#define USE_24HR_TIME false
+// Use 24 hour format for entering time, default=true
+#define USE_24HR_TIME true
 
 // THAT IS ALL FOR USER CONFIGURABLE PARAMETERS! Do not change anything below this line!
 // -------------------------------------------------------------------------------------
