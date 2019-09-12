2aef2061086e520228733bb54be9a139fa87a7b5
Howard Dutton <hjd1964@gmail.com>
2019-09-12 17:00:04 -0400

    Added some warnings/setting advice for TMC2130

--- src/stepper_drivers/TMC2130_INIT.h
+++ src/stepper_drivers/TMC2130_INIT.h
@@ -6,9 +6,13 @@
 #elif AXIS1_DRIVER_MODEL == TMC2130_LOWPWR
   #undef AXIS1_DRIVER_MODEL
   #define AXIS1_DRIVER_MODEL TMC_SPI
+  #if defined(AXIS1_TMC_IGOTO) || defined(AXIS1_TMC_IRUN) || defined(AXIS1_TMC_IHOLD)
+    #error "Configuration: AXIS1 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS1_TMC_IHOLD 1000 // 40% of Vref setting on trim-pot
   #define AXIS1_TMC_IRUN 1250  // 50%
   #define AXIS1_TMC_IGOTO 2500 // 100%
+  #define AXIS1_TMC_VREF
 #elif AXIS1_DRIVER_MODEL == TMC2130_QUIET
   #undef AXIS1_DRIVER_MODEL
   #define AXIS1_DRIVER_MODEL TMC_SPI
@@ -17,9 +21,13 @@
   #undef AXIS1_DRIVER_MODEL
   #define AXIS1_DRIVER_MODEL TMC_SPI
   #define AXIS1_TMC_MODE STEALTHCHOP
+  #if defined(AXIS1_TMC_IGOTO) || defined(AXIS1_TMC_IRUN) || defined(AXIS1_TMC_IHOLD)
+    #error "Configuration: AXIS1 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS1_TMC_IHOLD 1000
   #define AXIS1_TMC_IRUN 1250
   #define AXIS1_TMC_IGOTO 2500
+  #define AXIS1_TMC_VREF
 #elif AXIS1_DRIVER_MODEL == TMC2130_VQUIET
   #undef AXIS1_DRIVER_MODEL
   #define AXIS1_DRIVER_MODEL TMC_SPI
@@ -30,9 +38,21 @@
   #define AXIS1_DRIVER_MODEL TMC_SPI
   #define AXIS1_TMC_MODE STEALTHCHOP
   #define AXIS1_TMC_MODE_GOTO STEALTHCHOP
+  #if defined(AXIS1_TMC_IGOTO) || defined(AXIS1_TMC_IRUN) || defined(AXIS1_TMC_IHOLD)
+    #error "Configuration: AXIS1 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS1_TMC_IHOLD 1000
   #define AXIS1_TMC_IRUN 1250
   #define AXIS1_TMC_IGOTO 2500
+  #define AXIS1_TMC_VREF
+#endif
+
+#if !defined(AXIS1_TMC_VREF) && (defined(AXIS1_TMC_IRUN) && !defined(AXIS1_TMC_IGOTO) && !defined(AXIS1_TMC_IHOLD))
+  #warning "Configuration: AXIS1 TMC2130, use Vref=2.5V and be sure AXIS1_TMC_IRUN is set to match your stepper motor's current rating"
+#elif !defined(AXIS1_TMC_VREF) && (defined(AXIS1_TMC_IRUN) || defined(AXIS1_TMC_IGOTO) || defined(AXIS1_TMC_IHOLD))
+  #warning "Configuration: AXIS1 TMC2130, use Vref=2.5V and be sure AXIS1_TMC_IRUN, AXIS1_TMC_IGOTO, and AXIS1_TMC_IHOLD are all set properly for your stepper motor."
+#else
+  #warning "Configuration: AXIS1 TMC2130, set Vref to match your stepper motor's current rating."
 #endif
 
 #if AXIS2_DRIVER_MODEL == TMC2130
@@ -41,9 +61,13 @@
 #elif AXIS2_DRIVER_MODEL == TMC2130_LOWPWR
   #undef AXIS2_DRIVER_MODEL
   #define AXIS2_DRIVER_MODEL TMC_SPI
+  #if defined(AXIS2_TMC_IGOTO) || defined(AXIS2_TMC_IRUN) || defined(AXIS2_TMC_IHOLD)
+    #error "Configuration: AXIS2 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS2_TMC_IHOLD 1000
   #define AXIS2_TMC_IRUN 1250
   #define AXIS2_TMC_IGOTO 2500
+  #define AXIS2_TMC_VREF
 #elif AXIS2_DRIVER_MODEL == TMC2130_QUIET
   #undef AXIS2_DRIVER_MODEL
   #define AXIS2_DRIVER_MODEL TMC_SPI
@@ -52,9 +76,13 @@
   #undef AXIS2_DRIVER_MODEL
   #define AXIS2_DRIVER_MODEL TMC_SPI
   #define AXIS2_TMC_MODE STEALTHCHOP
+  #if defined(AXIS2_TMC_IGOTO) || defined(AXIS2_TMC_IRUN) || defined(AXIS2_TMC_IHOLD)
+    #error "Configuration: AXIS2 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS2_TMC_IHOLD 1000
   #define AXIS2_TMC_IRUN 1250
   #define AXIS2_TMC_IGOTO 2500
+  #define AXIS2_TMC_VREF
 #elif AXIS2_DRIVER_MODEL == TMC2130_VQUIET
   #undef AXIS2_DRIVER_MODEL
   #define AXIS2_DRIVER_MODEL TMC_SPI
@@ -65,7 +93,19 @@
   #define AXIS2_DRIVER_MODEL TMC_SPI
   #define AXIS2_TMC_MODE STEALTHCHOP
   #define AXIS2_TMC_MODE_GOTO STEALTHCHOP
+  #if defined(AXIS2_TMC_IGOTO) || defined(AXIS2_TMC_IRUN) || defined(AXIS2_TMC_IHOLD)
+    #error "Configuration: AXIS2 TMC2130, setting current directly is incompatible with _LOWPWR option."
+  #endif
   #define AXIS2_TMC_IHOLD 1000
   #define AXIS2_TMC_IRUN 1250
   #define AXIS2_TMC_IGOTO 2500
+  #define AXIS2_TMC_VREF
+#endif
+
+#if !defined(AXIS2_TMC_VREF) && (defined(AXIS2_TMC_IRUN) && !defined(AXIS2_TMC_IGOTO) && !defined(AXIS2_TMC_IHOLD))
+  #warning "Configuration: AXIS2 TMC2130, use Vref=2.5V and be sure AXIS2_TMC_IRUN to match your stepper motor's current rating."
+#elif !defined(AXIS2_TMC_VREF) && (defined(AXIS2_TMC_IRUN) || defined(AXIS2_TMC_IGOTO) || defined(AXIS2_TMC_IHOLD))
+  #warning "Configuration: AXIS2 TMC2130, use Vref=2.5V and be sure AXIS2_TMC_IRUN, AXIS2_TMC_IGOTO, and AXIS2_TMC_IHOLD are all set properly for your stepper motor."
+#else
+  #warning "Configuration: AXIS2 TMC2130, set Vref to match your stepper motor's current rating."
 #endif
