8ae9b78d235d5f13fbc5f8d6db961fdca09f66cc
Howard Dutton <hjd1964@gmail.com>
2019-10-07 07:24:56 -0400

    Improved comments

--- Config.h
+++ Config.h
@@ -21,10 +21,10 @@
                                           //         Ramps14, MaxESP2, MaxESP3. Others in Models.h are depreciated.
 
 // SERIAL PORTS --------------------------------------------------------------------------------------------------------------------
-#define SERIAL_A_BAUD_DEFAULT        9600 //   9600, Common baud rates for these parameters are 9600,19200,57600,115200.      Infreq
-#define SERIAL_B_BAUD_DEFAULT        9600 //   9600, See (src/HAL/) for your MCU Serial port # etc.                           Option
+#define SERIAL_A_BAUD_DEFAULT        9600 //   9600, n. Where n=9600,19200,57600,115200 (common baud rates.)                  Infreq
+#define SERIAL_B_BAUD_DEFAULT        9600 //   9600, n. See (src/HAL/) for your MCU Serial port # etc.                        Option
 #define SERIAL_B_ESP_FLASHING         OFF //    OFF, ON Upload ESP8266 WiFi firmware through SERIAL_B with :ESPFLASH# cmd.    Option
-#define SERIAL_C_BAUD_DEFAULT         OFF //    OFF, ON for ESP32 Bluetooth.                                                  Option
+#define SERIAL_C_BAUD_DEFAULT         OFF //    OFF, n, ON for ESP32 Bluetooth.                                               Option
 #define SERIAL_C_BLUETOOTH_NAME  "OnStep" // "On..", Bluetooth device name for ESP32.                                         Option
 
 // MOUNT TYPE --------------------------------------------------------------------------------------------------------------
