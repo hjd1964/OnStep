926b57658ccffb3ec69348d901fc0fdf293a03cc
Howard Dutton <hjd1964@gmail.com>
2019-10-11 10:23:35 -0400

    Tracking mode microsteps can only be 1 for servos

--- src/step_dir_drivers/Validate.SERVO.h
+++ src/step_dir_drivers/Validate.SERVO.h
@@ -1,8 +1,8 @@
 // SERVO driver validate
 
 #if AXIS1_DRIVER_MODEL == SERVO
-  #if AXIS1_DRIVER_MICROSTEPS != 1 && AXIS1_DRIVER_MICROSTEPS != 2 && AXIS1_DRIVER_MICROSTEPS != 4 && AXIS1_DRIVER_MICROSTEPS != 8 && AXIS1_DRIVER_MICROSTEPS != 16 && AXIS1_DRIVER_MICROSTEPS != 32 && AXIS1_DRIVER_MICROSTEPS != 64 && AXIS1_DRIVER_MICROSTEPS != 128
-    #error "Configuration (Config.h): AXIS1_DRIVER_MICROSTEPS; SERVO invalid digital gearing mode, use: 128,64,32,16,8,4,2,or 1"
+  #if AXIS1_DRIVER_MICROSTEPS != 1
+    #error "Configuration (Config.h): AXIS1_DRIVER_MICROSTEPS; SERVO invalid digital gearing mode, you must use: 1"
   #endif
   #if HAL_PULSE_WIDTH < SERVO_PULSE_WIDTH
     #error "Configuration (Config.h): STEP_WAVE_FORM PULSE; Pulse width is below the SERVO generic driver settings."
@@ -17,8 +17,8 @@
 #endif
 
 #if AXIS2_DRIVER_MODEL == SERVO
-  #if AXIS2_DRIVER_MICROSTEPS != 1 && AXIS2_DRIVER_MICROSTEPS != 2 && AXIS2_DRIVER_MICROSTEPS != 4 && AXIS2_DRIVER_MICROSTEPS != 8 && AXIS2_DRIVER_MICROSTEPS != 16 && AXIS2_DRIVER_MICROSTEPS != 32 && AXIS2_DRIVER_MICROSTEPS != 64 && AXIS2_DRIVER_MICROSTEPS != 128
-    #error "Configuration (Config.h): AXIS2_DRIVER_MICROSTEPS; SERVO invalid digital gearing mode, use: 128,64,32,16,8,4,2,or 1"
+  #if AXIS2_DRIVER_MICROSTEPS != 1
+    #error "Configuration (Config.h): AXIS2_DRIVER_MICROSTEPS; SERVO invalid digital gearing mode, you must use: 1"
   #endif
   #if HAL_PULSE_WIDTH < SERVO_PULSE_WIDTH
     #error "Configuration (Config.h): STEP_WAVE_FORM PULSE; Pulse width is below the SERVO generic driver settings."
