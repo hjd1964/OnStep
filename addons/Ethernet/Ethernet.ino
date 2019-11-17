eed8fee27bc672748d9ed11d1de1067f9582a366
Howard Dutton <hjd1964@gmail.com>
2019-11-17 08:27:37 -0500

    Upped patch level

--- addons/Ethernet/Ethernet.ino
+++ addons/Ethernet/Ethernet.ino
@@ -38,7 +38,7 @@
 #define FirmwareTime          __TIME__
 #define FirmwareVersionMajor  "1"
 #define FirmwareVersionMinor  "11"
-#define FirmwareVersionPatch  "b"
+#define FirmwareVersionPatch  "c"
 
 #define Version FirmwareVersionMajor "." FirmwareVersionMinor FirmwareVersionPatch
 
