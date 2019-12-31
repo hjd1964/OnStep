56750a921470371df2802b346e14ae2f85ea3ae0
Howard Dutton <hjd1964@gmail.com>
2019-12-31 09:23:03 -0500

    Upped patch level

--- addons/St4Serial/SmartHandController/SmartHandController.ino
+++ addons/St4Serial/SmartHandController/SmartHandController.ino
@@ -37,7 +37,7 @@
 #define FirmwareTime          __TIME__
 #define FirmwareVersionMajor  "1"
 #define FirmwareVersionMinor  "7"
-#define FirmwareVersionPatch  "a"
+#define FirmwareVersionPatch  "b"
 
 #include "Constants.h"
 #include "Config.h"
