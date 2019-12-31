ea3a44569f403a6815d19f90551346e89fa0a107
Howard Dutton <hjd1964@gmail.com>
2019-12-31 09:07:49 -0500

    Upped minor revision

--- addons/St4Serial/SmartHandController/SmartHandController.ino
+++ addons/St4Serial/SmartHandController/SmartHandController.ino
@@ -36,8 +36,8 @@
 #define FirmwareDate          __DATE__
 #define FirmwareTime          __TIME__
 #define FirmwareVersionMajor  "1"
-#define FirmwareVersionMinor  "6"
-#define FirmwareVersionPatch  "c"
+#define FirmwareVersionMinor  "7"
+#define FirmwareVersionPatch  "a"
 
 #include "Constants.h"
 #include "Config.h"
