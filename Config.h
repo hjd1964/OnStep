559999d5969b06131b1731baff9457522560993b
Howard Dutton <hjd1964@gmail.com>
2020-01-22 08:08:26 -0500

    Fixed typo, thanks Khalid

--- Config.h
+++ Config.h
@@ -102,7 +102,7 @@
 // MOTION CONTROL ----------------------------------------------------------------------------------------------------------
 #define STEP_WAVE_FORM             SQUARE // SQUARE, PULSE Step signal wave form faster rates. SQUARE best signal integrity.  Adjust
 
-// Stepper driver models (also see ~/OnStep/sd_drivers/Models.h for additional infrequently used models and more info.): 
+// Stepper driver models (also see ~/OnStep/src/sd_drivers/Models.h for additional infrequently used models and more info.): 
 // A4988, DRV8825, LV8729, S109, SSS TMC2209*, TMC2130* **, and TMC5160* ***
 // * = add _QUIET (stealthChop tracking) or _VQUIET (stealthChop tracking & slew,) for example "TMC2130_VQUIET"
 // ** = SSS TMC2130 if you choose to set stepper driver current (in mA) set Vref pot. 2.5V instead of by motor current as usual.
