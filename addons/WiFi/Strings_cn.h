a641c22cd42f3341b0e7d89446c63d884733039a
Howard Dutton <hjd1964@gmail.com>
2020-02-26 06:41:25 -0500

    Added cn "None"

--- addons/WiFi/Strings_cn.h
+++ addons/WiFi/Strings_cn.h
@@ -376,7 +376,7 @@
 // ------------------------- MountStatus -------------------------
 
 // general (background) errors
-#define L_GE_NONE "NONE"
+#define L_GE_NONE "不是一个"
 #define L_GE_MOTOR_FAULT "电机/驱动器错误"
 #define L_GE_ALT_MIN "地平线以下" 
 #define L_GE_LIMIT_SENSE "限位开关已触发"
