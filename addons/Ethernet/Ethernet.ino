92deb5ef19406f389db72b699954e5e23153a6a5
Howard Dutton <hjd1964@gmail.com>
2020-04-03 08:30:10 -0400

    Upped minor revision

--- addons/Ethernet/Ethernet.ino
+++ addons/Ethernet/Ethernet.ino
@@ -37,8 +37,8 @@
 #define FirmwareDate          __DATE__
 #define FirmwareTime          __TIME__
 #define FirmwareVersionMajor  "1"
-#define FirmwareVersionMinor  "12"
-#define FirmwareVersionPatch  "g"
+#define FirmwareVersionMinor  "13"
+#define FirmwareVersionPatch  "a"
 
 #define Version FirmwareVersionMajor "." FirmwareVersionMinor FirmwareVersionPatch
 
