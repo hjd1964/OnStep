38c087e9dab0b094dc67ad55680f6f860e23187e
Howard Dutton <hjd1964@gmail.com>
2020-07-03 16:55:26 -0400

    Better comment

--- src/pinmaps/Pins.FYSETC_S6.h
+++ src/pinmaps/Pins.FYSETC_S6.h
@@ -3,7 +3,7 @@
 
 #if defined(STM32F446xx)
 
-// TX2 (PA2/PA3) is on the Y+ and Z+ end stops and is reserved for GPS (etc, no command channel is associated with this port)
+// RX2/TX2 (PA2/PA3) is on the Y+ and Z+ end stops and is reserved for GPS (etc, no command channel is associated with this port)
 
 // Temperature sensors (From Marlin) perhaps these would make better Limit sw and 2x home switches, and PEC.
 // They have built-in pullups and a 10uf cap for ESD but they are not labeled as such so leaving it be.
