3e3857691ed1db88b06ad9e885a92959b32934dd
Howard Dutton <hjd1964@gmail.com>
2020-09-14 16:23:31 -0400

    Better comment

--- Config.h
+++ Config.h
@@ -107,7 +107,7 @@
 
 // Stepper driver models (also see ~/OnStep/src/sd_drivers/Models.h for additional infrequently used models and more info.): 
 // A4988, DRV8825, LV8729, S109, SSS TMC2209*, TMC2130* **, and TMC5160* ***
-// * = add _QUIET (stealthChop tracking) or _VQUIET (stealthChop tracking & slew,) for example "TMC2130_QUIET"
+// * = add _QUIET (stealthChop tracking) for example "TMC2130_QUIET"
 // ** = SSS TMC2130 if you choose to set stepper driver current (in mA) set Vref pot. 2.5V instead of by motor current as usual.
 // *** = SSS TMC5160 you must set stepper driver current (in mA) w/ #define AXISn_TMC_IRUN (IHOLD, etc.)
 
