b286d065b40e0f48aec0fc8724f9cfebd15e37dd
Howard Dutton <hjd1964@gmail.com>
2020-10-04 08:45:48 -0400

    Experiment for 5160 drivers in S6

--- src/HAL/HAL_STM32F4/HAL_STM32F4.h
+++ src/HAL/HAL_STM32F4/HAL_STM32F4.h
@@ -276,8 +276,8 @@ void QuickSetIntervalAxis2(uint32_t r) {
 #define a2DIR_H digitalWriteFast(Axis2_DIR, HIGH)
 #define a2DIR_L digitalWriteFast(Axis2_DIR, LOW)
 
-// fast bit-banged SPI should hit an ~1 MHz bitrate for TMC drivers
-#define delaySPI delayNanoseconds(500)
+// fast bit-banged SPI should hit an ~0.5 MHz bitrate for TMC drivers
+#define delaySPI delayMicroseconds(1)
 
 #define a1CS_H digitalWriteFast(Axis1_M2,HIGH)
 #define a1CS_L digitalWriteFast(Axis1_M2,LOW)
