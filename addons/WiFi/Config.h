067343ebd5b8ec7b6182e38c5f0eef39e167364b
Howard Dutton <hjd1964@gmail.com>
2020-10-21 11:48:48 -0400

    Corrected some defaults

--- addons/WiFi/Config.h
+++ addons/WiFi/Config.h
@@ -18,7 +18,7 @@
 
 //      Parameter Name              Value   Default  Notes                                                                      Hint
 // SERIAL PORTS --------------------------------------------------------------------------------------------------------------------
-#define SERIAL_BAUD_DEFAULT         57600 //   9600, Common baud rates for these parameters are 9600,19200,57600,115200.      Infreq
+#define SERIAL_BAUD_DEFAULT          9600 //   9600, Common baud rates for these parameters are 9600,19200,57600,115200.      Infreq
 #define SERIAL_BAUD                 57600 //  57600, Automatically uses 19200 if talking to a Mega2560 OnStep.                Infreq
                                           //         If establishing a link to OnStep was ***unsuccessful*** the ESP8266 may
                                           //         retain prior settings perhaps an SSID from factory defaults, for example.
