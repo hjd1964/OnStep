db18e4d7e1148e9afa5bf222c4b1dc46da3d1fa8
Howard Dutton <hjd1964@gmail.com>
2021-01-23 14:11:37 -0500

    Upped patch level

--- addons/SmartHandController/SmartHandController.ino
+++ addons/SmartHandController/SmartHandController.ino
@@ -37,7 +37,7 @@
 #define FirmwareTime          __TIME__
 #define FirmwareVersionMajor  "1"
 #define FirmwareVersionMinor  "9"
-#define FirmwareVersionPatch  "j"
+#define FirmwareVersionPatch  "k"
 
 #include "Constants.h"
 #include "Locales.h"
