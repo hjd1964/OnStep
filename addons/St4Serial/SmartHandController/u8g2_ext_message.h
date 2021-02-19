6585d1e6a8ed6d7e9859bde1ed92c1c97badc51c
Howard Dutton <hjd1964@gmail.com>
2021-02-19 07:02:22 -0500

    Updated U8G2 library compatibility

--- addons/St4Serial/SmartHandController/u8g2_ext_message.h
+++ addons/St4Serial/SmartHandController/u8g2_ext_message.h
@@ -1,4 +1,4 @@
 #pragma once
 #include <U8g2lib.h>
 #include "u8g2_ext_event.h"
-uint8_t ext_UserInterfaceMessage(u8g2_t *u8g2, Pad *extPad,const char *title1, const char *title2, const char *title3, const char *buttons);
+u8g2_uint_t ext_UserInterfaceMessage(u8g2_t *u8g2, Pad *extPad,const char *title1, const char *title2, const char *title3, const char *buttons);
