6759b61110642e600cfc5b3be7af133f22fe006e
Howard Dutton <hjd1964@gmail.com>
2021-03-25 10:48:42 -0400

    Refactored webserver

--- addons/Ethernet/WebServer.h
+++ addons/Ethernet/WebServer.h
@@ -1,9 +1,24 @@
 // -----------------------------------------------------------------------------------
 // Web server
 
+// Turn ON to allow webserver debug messages
+#define WEBSERVER_DEBUG OFF
+
+#if WEBSERVER_DEBUG == ON
+  #define W(x) V(x)
+  #define WF(x) VF(x)
+  #define WL(x) VL(x)
+  #define WLF(x) VLF(x)
+#else
+  #define W(x)
+  #define WF(x)
+  #define WL(x)
+  #define WLF(x)
+#endif
+
 // misc.
 #define WebSocketTimeOut 10000
-#define WEBHANDLERCOUNT     21
+#define WEB_HANDLER_COUNT     21
 
 typedef void (* webFunction) (EthernetClient *client);
 
@@ -13,31 +28,31 @@ class WebServer {
     void handleClient();
     void setResponseHeader(const char *str);
     void on(String fn, webFunction handler);
-#if SD_CARD == ON
-    void on(String fn);
-#endif
+    #if SD_CARD == ON
+      void on(String fn);
+    #endif
     void onNotFound(webFunction handler);
     String arg(String id);
 
     bool SDfound=false;
   private:
-#if SD_CARD == ON
-    void sdPage(String fn, EthernetClient *client);
-#endif
+    #if SD_CARD == ON
+      void sdPage(String fn, EthernetClient *client);
+    #endif
 
     String inputBuffer;
     char responseHeader[200]="";
-#if SD_CARD == ON
-    bool modifiedSinceFound=false;
-#endif
+    #if SD_CARD == ON
+      bool modifiedSinceFound=false;
+    #endif
 
     webFunction notFoundHandler=NULL;
-    webFunction handlers[WEBHANDLERCOUNT];
-    String handlers_fn[WEBHANDLERCOUNT];
+    webFunction handlers[WEB_HANDLER_COUNT];
+    String handlers_fn[WEB_HANDLER_COUNT];
     int handler_count;
     
-    String parameters[WEBHANDLERCOUNT];
-    String values[WEBHANDLERCOUNT];
+    String parameters[WEB_HANDLER_COUNT];
+    String values[WEB_HANDLER_COUNT];
     int parameter_count;
 };
 
